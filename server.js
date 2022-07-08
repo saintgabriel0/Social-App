c1ba724314fbcabfbac1cd9afaf099330a578d82
ChineduG <investorgbriel@gmail.com>
2022-07-08 11:46:59 +0100

    .

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
-const express = require('express');
+// The reason I can't use const here is, 
+// I use type = module on the package.json file
+// ! const express = require('express');
+// ! const bodyParser = require('body-parser')
+
+// * I'll be using import to get all dependencies
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 // Middleware 
-app.use(express.urlencoded());
+app.use(bodyParser.json({limit: '30mb', extended: true}))
+app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
 app.use(express.json());
 
 app.get('/hello', (req, res) => {
